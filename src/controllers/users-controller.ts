3285f715f77a3e92f15b80757c8c89b9bbf7857a
eomgn <matheeusgn21@gmail.com>
2025-07-23 21:58:41 -0300

    criando arquivo de configuracao prisma.ts para conexao com o banco de dados alem de tambem fazer o metodo create em users-controller enviar dados para o banco de dados

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,49 @@
 import { Request, Response } from "express";
 
+// import PRISMA da pasta database
+import { prisma } from "@/database/prisma";
+
+// import BCRYPT
+import { hash } from "bcrypt";
+
+// import ZOD
+import zod from "zod";
+
+// import APPERROR
+import { AppError } from "@/utils/AppError";
+
 export class UsersController {
-  create(request: Request, response: Response) {
-    return response.status(200).json({ message: "API ok!" });
+  async create(request: Request, response: Response) {
+    const bodySchema = zod.object({
+      name: zod.string().trim().min(3),
+      email: zod.string().email(),
+      password: zod.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
+    // recuperando primeiro 'email' encontrado do que foi passado
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+
+    // agora verificando se o 'email' ja existe no banco e se sim retornar uma excessão
+    if (userWithSameEmail) {
+      throw new AppError("User with the same email already exists");
+    }
+
+    // criptografia password
+    const hashedPassword = await hash(password, 8);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    // userWhitoutPassword = USUÁRIO SEM A SENHA --> o SPREAD OPERATOR é para retornar os demais dados
+    const { password: _, ...userWhitoutPassword } = user;
+
+    return response.json(userWhitoutPassword);
   }
 }
