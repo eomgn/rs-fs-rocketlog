cbfdfc4b4ca3440842a89d983b74ce40952412b4
eomgn <matheeusgn21@gmail.com>
2025-07-28 19:24:29 -0300

    método create em deliveries controller

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from "express";
+import { prisma } from '@/database/prisma'
+import zod from 'zod'
 
 export class DeliveriesController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+  async create(request: Request, response: Response) {
+    const bodySchema = zod.object({
+      user_id: zod.string().uuid(),
+      description: zod.string()
+    })
+
+    const { user_id, description } = await bodySchema.parse(request.body)
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description
+      }}
+    )
+
+    return response.status(201).json();
   }
 }
