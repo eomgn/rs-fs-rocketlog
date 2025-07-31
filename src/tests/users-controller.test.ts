11c75e4e0b335363add983e95b66c5df11c2be33
eomgn <matheeusgn21@gmail.com>
2025-07-30 22:56:21 -0300

    criando teste para cadastro de novos usuarios

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,6 +1,18 @@
+import request from "supertest";
+
+import { app } from "@/app";
+
 describe("UsersController", () => {
   // criando teste
-  it("test", () => {
-    console.log("Passou por aqui");
+  it("should create a new user successfully", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Test User",
+      email: "testuser@email.com",
+      password: "123456",
+    });
+
+    expect(response.status).toBe(201); // verificando se retorna status "201"
+    expect(response.body).toHaveProperty("id"); // verificando se há a propriedade "id"
+    expect(response.body.name).toBe("Test User"); // verificando se o .name é exatamente "Test User"
   });
 });
