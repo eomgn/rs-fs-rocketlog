8e1806ae2110fe25edbabb3a1f3b5cd7b63247d6
eomgn <matheeusgn21@gmail.com>
2025-08-01 22:18:56 -0300

    remove usuario criado para teste de autenticacao sessions

--- src/tests/sessions-controller.test.ts
+++ src/tests/sessions-controller.test.ts
@@ -2,7 +2,16 @@ import request from "supertest";
 
 import { app } from "@/app";
 
+import { prisma } from "@/database/prisma";
+
 describe("SessionsController", () => {
+  let user_id: string;
+
+  //   afterAll que executa depois do teste
+  afterAll(async () => {
+    await prisma.user.delete({ where: { id: user_id } });
+  });
+
   it("should authenticate a and get access token", async () => {
     // criando um usuário
     const user = await request(app).post("/users").send({
@@ -10,6 +19,9 @@ describe("SessionsController", () => {
       email: "authtestuser@email.com",
       password: "123456",
     });
+
+    // capturando 'id' do usuário criado para teste
+    user_id = user.body.id;
 
     // criando sessão para verificar a atuenticacao
     const sessions = await request(app).post("/sessions").send({
