3c7bf5ed8d6545f0754b1558941c57a8e7e24e28
eomgn <matheeusgn21@gmail.com>
2025-08-04 11:23:01 -0300

    criando variavel de ambiente para PORT

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import zod from "zod";
 const envSchema = zod.object({
   DATABASE_URL: zod.string().url(),
   JWT_SECRET: zod.string(),
+  PORT: zod.coerce.number().default(3333), // '.coerce' é para transformar em number caso seja passado string
 });
 
 // criando e exportando a variável env e passando o schema que recebe process.env que é por onde está passando todas as variáveis de ambiente e validando elas
